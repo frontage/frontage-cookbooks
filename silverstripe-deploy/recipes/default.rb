b85f08dbf004a916cae4b7c01852b5566d789f68
Zac Evans <zac@node.co.nz>
2013-03-11 12:40:19 +1300

    recipe update

--- silverstripe-deploy/recipes/default.rb
+++ silverstripe-deploy/recipes/default.rb
@@ -2,23 +2,29 @@ node[:deploy].each do |application, deploy|
 
 	Chef::Log.debug("deploy::php application #{application} #{deploy}")
 
-	bash "setup silverstripe assets" do
-	  code <<-EOH
-	  mkdir #{deploy[:deploy_to]}/test_folder
-	  EOH
+	#bash "setup silverstripe assets" do
+	#  code <<-EOH
+	#  mkdir #{deploy[:deploy_to]}/test_folder
+	#  EOH
 
-	end
+	#end
 
 	directory "assets" do
 		path "/vol/assets/#{application}"
 		mode 0777
 		action :create
+		not_if do
+    		::File.exists?("/vol/assets/#{application}")
+  		end
 	end
 
 	link "assets" do
-		target_file "#{deploy[:deploy_to]}/current/assets"
+		target_file "#{deploy[:absolute_document_root]}/assets"
 		to "/vol/assets/#{application}"
 		action :create
+		not_if do
+    		::File.exists?("#{deploy[:absolute_document_root]}/assets")
+  		end
 	end
 
 end
