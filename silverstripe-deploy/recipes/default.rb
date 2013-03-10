e29a2c1d9cbe4eaf80bd0bc70c0e8380d15f48e4
Zac Evans <zac@node.co.nz>
2013-03-11 10:01:32 +1300

    update recipe

--- silverstripe-deploy/recipes/default.rb
+++ silverstripe-deploy/recipes/default.rb
@@ -1,1 +1,7 @@
-package("vim")
+bash "really awesome way to create a mysql database from chef using the bash method" do
+	# a heredoc of the code to execute, note the node hash is created from the JSON file
+  code <<-HEY_BRO_EOM
+  mkdir /srv/
+  HEY_BRO_EOM
+
+end
