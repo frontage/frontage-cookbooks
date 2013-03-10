628c72a898837a30418ede5e5e95fa94742a0ff5
Zac Evans <zac@node.co.nz>
2013-03-11 11:59:16 +1300

    recipe

--- silverstripe-deploy/recipes/default.rb
+++ silverstripe-deploy/recipes/default.rb
@@ -5,7 +5,7 @@ node[:deploy].each do |application, deploy|
 	bash "really awesome way to create a mysql database from chef using the bash method" do
 		# a heredoc of the code to execute, note the node hash is created from the JSON file
 	  code <<-EOH
-	  mkdir {deploy[:deploy_to]}/test_folder
+	  mkdir #{deploy[:deploy_to]}/test_folder
 	  EOH
 
 	end
